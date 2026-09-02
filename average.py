9fa89d6f14427c0c05982fa715b51a9c7b5b857c
Akanksha <akankshasingh2as7@gmail.com>
2026-09-02 19:05:38 +0530

    Add average Python program

---
+++ average.py
@@ -0,0 +1,5 @@
+firstno=int(input("Enter first number:"))
+secondno=int(input("Enter the second number:"))
+thirdno=int(input("Enter third number:"))
+Average=(firstno+secondno+thirdno)/3
+print(f"The average of {firstno}, {secondno}, {thirdno} is:",Average)
