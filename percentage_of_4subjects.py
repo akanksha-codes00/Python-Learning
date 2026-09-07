4b9fb076ece8b87ceb518dd525064a5838f2527f
Akanksha <akankshasingh2as7@gmail.com>
2026-09-07 15:56:48 +0530

    Percentage of 4 Subjects by taking input from user

---
+++ percentage_of_4subjects.py
@@ -0,0 +1,6 @@
+subject1=float(input("Enter the marks of  first subject :"))
+subject2=float(input("Enter the marks of second subject :"))
+subject3=float(input("Enter the marks of third subject :"))
+subject4=float(input("Enter the marks of fourth subject :"))
+Total=(subject1+subject2+subject3+subject4)/400*100
+print("The percentage of 4  subjects is:",Total)
