b60c3d55e0ff20ac8d5f863f1f71ec17f3df6562
Akanksha <akankshasingh2as7@gmail.com>
2026-09-07 15:35:26 +0530

    Swap 2 values with 3rd varriable

---
+++ swapping_with_thirdvarriable.py
@@ -0,0 +1,7 @@
+firstno=int(input("Enter the first number:"))
+secondno=int(input("Enter the second number:"))
+thirdno=firstno
+firstno=secondno
+secondno=thirdno
+print("After swapping  first number is:",firstno)
+print("After swapping second number is:",secondno)
