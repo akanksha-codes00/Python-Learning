b9374e9da1588da8fc43a06e8ea4816035bdebba
Akanksha <akankshasingh2as7@gmail.com>
2026-09-08 15:35:14 +0530

    Positive Difference between 2 values by taking input from user

---
+++ positive_difference_2values.py
@@ -0,0 +1,8 @@
+firstno=int(input("Enter the first number:"))
+secondno=int(input("Enter the second number:"))
+if(firstno>secondno):
+    difference=firstno-secondno
+    print("The positive difference between two numbers is:", difference)
+else:
+    difference=secondno-firstno
+    print("The positive difference between two numbers is:",difference)
