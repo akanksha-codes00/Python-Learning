8189beb47a027bc3f650875ef58540027490a45d
Akanksha <akankshasingh2as7@gmail.com>
2026-09-09 17:50:56 +0530

    The Maximum number within two values by taking input from user

---
+++ maximum_within2values.py
@@ -0,0 +1,6 @@
+firstno=int(input("Enter the first number:"))
+secondno=int(input("Enter the second number:"))
+if(firstno>secondno):
+    print("The maximum number between two numbers is:",firstno)
+else:
+    print("The maximum number between two numbers is:",secondno)
