b800a8c5037a83561a7606535d653791bdaee049
Akanksha <akankshasingh2as7@gmail.com>
2026-09-09 17:57:52 +0530

    Print Sqaure for entered Odd Number and Cube for even number

---
+++ cubeodd_squareeven.py
@@ -0,0 +1,7 @@
+number=int(input("Enetr a number:"))
+if(number%2==0):
+    cube=number*number*number
+    print("The cube of the even number is:",cube)
+else:
+    square=number*number
+    print("The square of the odd number is:",square)
