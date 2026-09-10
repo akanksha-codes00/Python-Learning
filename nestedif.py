d8e17c1a0c26ce26cd7d3387e41fffc315cda3a8
Akanksha <akankshasingh2as7@gmail.com>
2026-09-10 16:55:21 +0530

    Check entered number is positive even / negative odd number

---
+++ nestedif.py
@@ -0,0 +1,12 @@
+number=int(input("Enter a number:"))
+if(number>0):
+    if(number%2==0):
+        print("The number is positive and even:")
+    else:
+        print("The number is positive and odd:")
+else:
+    if(number%2==0):
+        print("The number is negative and even:")
+    else:
+        print("The number is negative and odd:")
+        
