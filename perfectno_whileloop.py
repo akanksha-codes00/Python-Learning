05ee78e2d0e5fa6e5da78fd3f3cf571d601f7c6b
Akanksha <akankshasingh2as7@gmail.com>
2026-09-19 16:35:47 +0530

    Print Fibonacci Series with the use of While Loop till term by taking term from user

---
+++ perfectno_whileloop.py
@@ -0,0 +1,3 @@
+sum=0
+number=int(input("Enter the number: "))
+unmber
--- whileloop_fibonacci.py
+++ whileloop_fibonacci.py
@@ -2,11 +2,13 @@ term=int(input("Enter the term:"))
 firstno=0
 secondno=1
 print("Fibonacci series is:")
-print(firstno, end=" ")
-print(secondno, end=" ")
-for i in range(1,term+1):
+print(firstno,secondno, end=" ")
+i=1
+
+while(i<=term):
     nextno=firstno+secondno
     firstno=secondno
     secondno=nextno
     print(nextno, end=" ")
+    i=i+1
 
