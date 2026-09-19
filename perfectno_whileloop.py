ea952b7d4a26ccde202e60ab1c198990a1f8c56f
Akanksha <akankshasingh2as7@gmail.com>
2026-09-19 16:54:43 +0530

    Check given Number is Perfect or Not

--- perfectno_whileloop.py
+++ perfectno_whileloop.py
@@ -1,3 +1,12 @@
-sum=0
+
 number=int(input("Enter the number: "))
-unmber
+sum=0
+i=1
+while(i<number):
+    if(number%i==0):
+        sum=sum+i
+    i=i+1
+if(sum==number):
+    print("The nunber is a perfect number")
+else:
+    print("The number is not a perfect number")
