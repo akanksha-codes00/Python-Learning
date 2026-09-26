a12fbac06c8a4fca3f3bbeb51638e00bdaee5eb5
Akanksha <akankshasingh2as7@gmail.com>
2026-09-26 20:07:32 +0530

    Print pattern type like 1 1 2 1 2 3 1 2 3 5

---
+++ pattern6.py
@@ -0,0 +1,11 @@
+firstno=0
+secondno=1
+for i in range(1,5):
+    for j in range(1,i+1,1):  
+        nextno=firstno+secondno
+        firstno=secondno
+        secondno=nextno
+        print(nextno,end=" ")
+    print()
+    firstno=0
+    secondno=1
